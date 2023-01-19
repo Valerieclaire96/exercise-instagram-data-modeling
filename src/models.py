8ad569a85ab99aba4d9554032958fb64adcd388f
Valerieclaire96 <Valerieclaire96@gmail.com>
2023-01-19 01:20:42 +0000

    instagram database

--- src/models.py
+++ src/models.py
@@ -8,26 +8,29 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class User(Base):
+        __tablename__ = 'user'
+        id = Column(Integer, primary_key=True)
+        name = Column(String(250), nullable=False)
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+        __tablename__ = 'post'
+        id = Column(Integer, primary_key=True)
+        name = Column(String(250), nullable=False)
+        post_id = Column(Integer, ForeignKey("user.id"))
+
+class Comment(Base):
+        __tablename__ = 'comment'
+        id = Column(Integer, primary_key=True)
+        name = Column(String(250), nullable=False)
+        comment = Column(Integer, ForeignKey("post.id"))
+
+class Reaction(Base):
+        __tablename__ = 'reaction'
+        id = Column(Integer, primary_key=True)
+        name = Column(String(250), nullable=False)
+        reaction_id = Column(Integer, ForeignKey("post.id"))
 
 ## Draw from SQLAlchemy base
 try:
